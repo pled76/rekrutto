6f5bf8d10ae306192485dc4b00559e3157cc9ec1
Pavel Ledkovsky <pled76@yandex.ru>
2020-04-07 20:38:20 +0300

    DEV get data for profile INCOMPLETE FEAT

--- api/backend.js
+++ api/backend.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+
 export default {
 
   useBack: 'https://test-i-staff.pled76.ru',
+  tokenPromise: null,
 
   doLogin(usr, pwd, store) {
     return axios.post(this.useBack + '/login', { login: usr, password: pwd })
-      .then(() => {
+      .then(resp => {
+        console.log(resp)
+        this.tokenPromise = axios.get(this.useBack + '/token')
         store.commit('login', usr)
       })
       .catch(() => {
@@ -17,5 +22,15 @@ export default {
   doLogout(store) {
     store.commit('logout')
     axios.get(this.useBack + '/logout')
+  },
+
+  readData(bo, store) {
+    if (!store.state.isLoggedIn || !this.tokenPromise)
+      return Promise.reject(new Error('not loged in'))
+    return this.tokenPromise
+      .then(resp => {
+        console.log(resp)
+        axios.get(this.useBack + '/data/' + bo)
+      })
   }
 }
---
+++ pages/profile.vue
@@ -0,0 +1,35 @@
+<template>
+  <FormPage>
+    <div class="slogan">
+      <div class="slogan__word">Система автоматизации</div>
+      <div class="slogan__word">подбора персонала</div>
+      <p class="slogan__text golden">для тех, кто ценит свое время</p>
+    </div>
+  </FormPage>
+</template>
+
+<script>
+  import FormPage from '@/components/FormPage'
+  import backend from '@/api/backend'
+
+  export default {
+    data() {
+      return {
+        backend
+      }
+    },
+
+    mounted () {
+      backend.readData('InfoMyRoles', this.$store)
+      .then(resp => {
+        console.log(resp.data)
+      })
+      .catch(e => {
+        console.log(e)
+        this.$router.push('/login')
+      })
+    },
+
+    components: { FormPage }
+  }
+</script>
