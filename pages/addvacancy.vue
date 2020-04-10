1dc8a646dfb74441c259a09ad16ea01589c0159f
Pavel Ledkovsky <pled76@yandex.ru>
2020-04-10 17:14:55 +0300

    FEAT addvacancy

---
+++ pages/addvacancy.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataPage mainHead="Добавить вакансию" :mainData="pageData" useSvg="Добавить вакансии" ></DataPage>
+</template>
+
+<script>
+  import DataPage from '../components/DataPage'
+  import pageData from '../schemes/addvacancy'
+
+  export default {
+
+    data() {
+      return {
+        pageData
+      }
+    },
+
+    components: { DataPage }
+  }
+</script>
