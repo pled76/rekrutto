913d022987fb84bf09a04dca9074e2fb6317f48b
Pavel Ledkovsky <pled76@yandex.ru>
2020-04-10 16:22:34 +0300

    FEAT addcandidate

---
+++ pages/addcandidate.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataPage mainHead="Добавить резюме" :mainData="pageData" ></DataPage>
+</template>
+
+<script>
+  import DataPage from '../components/DataPage'
+  import pageData from '../schemes/addcandidate'
+
+  export default {
+
+    data() {
+      return {
+        pageData
+      }
+    },
+
+    components: { DataPage }
+  }
+</script>
