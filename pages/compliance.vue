e747ffd937095152c0eb0b9d77b12b3f3496f21b
Pavel Ledkovsky <pled76@yandex.ru>
2020-04-10 15:04:20 +0300

    FEAT compliance and smartexchange

---
+++ pages/compliance.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataPage mainHead="Безопасность" :mainData="pageData" ></DataPage>
+</template>
+
+<script>
+  import DataPage from '../components/DataPage'
+  import pageData from '../schemes/compliance'
+
+  export default {
+
+    data() {
+      return {
+        pageData
+      }
+    },
+
+    components: { DataPage }
+  }
+</script>
---
+++ pages/smartexchange.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataPage mainHead="Умный обмен" :mainData="pageData" ></DataPage>
+</template>
+
+<script>
+  import DataPage from '../components/DataPage'
+  import pageData from '../schemes/smartexchange'
+
+  export default {
+
+    data() {
+      return {
+        pageData
+      }
+    },
+
+    components: { DataPage }
+  }
+</script>
