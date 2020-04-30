33ccd917c46724439a76a3682145a4afcfe59036
Pavel Ledkovsky <pled76@yandex.ru>
2020-04-30 18:32:37 +0300

    FEAT confirmation pages

---
+++ pages/okactivated.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataPage mainHead="Подтверждение прошло успешно!" :mainData="pageData" useSvg="Функциональность" ></DataPage>
+</template>
+
+<script>
+  import DataPage from '../components/DataPage'
+  import pageData from '../schemes/okactivated'
+
+  export default {
+
+    data() {
+      return {
+        pageData
+      }
+    },
+
+    components: { DataPage }
+  }
+</script>
