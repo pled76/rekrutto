514dab5c0daf9ce02968a65cb24c4be56b6573f4
Pavel Ledkovsky <pled76@yandex.ru>
2020-05-21 13:38:16 +0300

    FEAT раздельные формы для регистрации и логина

--- pages/login.vue
+++ pages/login.vue
@@ -5,42 +5,53 @@
         <div class="one__col half__width">
           <div class="form__title golden">РЕГИСТРАЦИЯ</div>
           <div>Введите адрес своей корпоративной почты и придумайте ПАРОЛЬ.</div>
-          <div>Запомните и сохраните.</div>
-          <div>Нажмите ссылку Подтвердить, появится кнопка Войти.</div>
-          <br>
-          <div>Далее необходимо просто пройти по ссылке, которую вы получите на указанную почту, и Ваш аккаунт ГОТОВ!</div>
+          <div>Нажмите Отправить.</div>
+          <form>
+            <div class="row">
+              <input v-model="login0" placeholder="Адрес рабочей почты" autocomplete="username" @keyup.enter="doLogin(0)" />
+            </div>
+            <div class="row">
+              <input v-model="pwd0" placeholder="Пароль" autocomplete="current-password" :type="pwdType0" @keyup.enter="doLogin(0)" />
+            </div>
+            <div class="row">
+              <a href="#" class="little__text" @click="pwdShowHide(0)">{{ showHidePwd0 }}</a>
+            </div>
+          </form>
+          <div v-if="loginError0" class="row red__text">
+            {{loginError0}}
+          </div>
+          <button v-if="confirmed0" @click="doLogin(0)" >Отправить</button>
         </div>
         <div class="one__col half__width">
           <div class="form__title golden">ВХОД В СИСТЕМУ</div>
-          <div>Введите адрес своей корпоративной почты.</div>
+          <div>Введите свою почту и пароль.</div>
           <br>
-          <div>Введите ранее указанный пароль. Нажмите ссылку Подтвердить, появится кнопка Войти.</div>
+          <div>Нажмите Подтвердить, далее нажмите Войти.</div>
+          <form>
+            <div class="row">
+              <input v-model="login1" placeholder="Адрес рабочей почты" autocomplete="username" @keyup.enter="doLogin(1)" />
+            </div>
+            <div class="row">
+              <input v-model="pwd1" placeholder="Пароль" autocomplete="current-password" :type="pwdType1" @keyup.enter="doLogin(1)" />
+            </div>
+            <div class="row">
+              <a href="#" class="little__text" @click="pwdShowHide(1)">{{ showHidePwd1 }}</a>
+            </div>
+          </form>
+          <div v-if="loginError1" class="row red__text">
+            {{loginError1}}
+          </div>
+          <button v-if="confirmed1" @click="doLogin(1)" >Войти</button>
+          <div class="row pad__top">
+            <a class="golden" href="#" @click="confirmed1 = !confirmed1">{{ confirmText(1) }}</a>
+          </div>
         </div>
       </div>
-      <form>
-        <div class="row">
-          <input v-model="login" placeholder="Адрес рабочей почты" autocomplete="username" @keyup.enter="doLogin()" />
-        </div>
-        <div class="row">
-          <input v-model="pwd" placeholder="Пароль" autocomplete="current-password" :type="pwdType" @keyup.enter="doLogin()" />
-        </div>
-        <div class="row">
-          <a href="#" class="little__text" @click="pwdShowHide()">{{showHidePwd}}</a>
-        </div>
-      </form>
       <div class="one_col">
-        <div v-if="loginError" class="row red__text">
-          {{loginError}}
-        </div>
-        <button v-if="confirmed" @click="doLogin()" >Войти</button>
-        <div class="row pad__top">
-          <a class="golden" href="#" @click="confirmed = !confirmed">{{ confirmText }}</a>
-        </div>
         <div class="row">
-          <a href="/confirmation" class="little__text pad__top">своё согласие с политикой использования ПД</a>
+          <a href="/confirmation" class="little__text pad__top">Согласие с политикой использования ПД</a>
         </div>
       </div>
-      <div class="form__title golden large_text">ВМЕСТЕ МЫ МОЖЕМ БОЛЬШЕ</div>
     </div>
   </FormPage>
 </template>
@@ -52,29 +63,37 @@
   export default {
     data() {
       return {
-        login: '',
-        pwd: '',
+        login0: '',
+        login1: '',
+        pwd0: '',
+        pwd1: '',
         backend,
-        pwdType: 'password',
-        showHidePwd: 'показать пароль',
-        loginError: '',
-        confirmed: false
+        pwdType0: 'password',
+        pwdType1: 'password',
+        showHidePwd0: 'показать пароль',
+        showHidePwd1: 'показать пароль',
+        loginError0: '',
+        loginError1: '',
+        confirmed0: true,
+        confirmed1: false
       }
     },
 
     methods: {
-      doLogin() {
-        if (!this.confirmed) {
-          this.loginError = 'Нажмите ссылку Подтвердить'
+      doLogin(i) {
+        const cfmd = 'confirmed' + i;
+        const lerr = 'loginError' + i;
+        if (!this[cfmd]) {
+          this.$set(this, lerr, 'Нажмите ссылку Подтвердить')
           return
         }
-        this.loginError = ''
-        backend.doLogin(this.login, this.pwd, this.$store)
+        this.$set(this, lerr, '')
+        backend.doLogin(this['login' + i], this['pwd' + i], this.$store)
           .then(() => {
             if (this.$store.state.isLoggedIn) {
               this.$nextTick(() => {
-                this.login = ''
-                this.pwd = ''
+                this.$set(this, 'login' + i, '')
+                this.$set(this, 'pwd' + i, '')
                 this.$router.push('/')
                 this.backend.openAt()
               })
@@ -82,24 +101,22 @@
             return this.$store.state.isLoggedIn
           })
           .catch(e => {
-            this.loginError = e.response.data
+            this.$set(this, lerr, e.response.data)
           })
       },
 
-      pwdShowHide() {
-        if (this.pwdType) {
-          this.pwdType = ''
-          this.showHidePwd = 'скрыть пароль'
+      pwdShowHide(i) {
+        if (this['pwdType' + i]) {
+          this.$set(this, 'pwdType' + i, '')
+          this.$set(this, 'showHidePwd' + i, 'скрыть пароль')
         } else {
-          this.pwdType = 'password'
-          this.showHidePwd = 'показать пароль'
+          this.$set(this, 'pwdType' + i, 'password')
+          this.$set(this, 'showHidePwd' + i, 'показать пароль')
         }
-      }
-    },
+      },
 
-    computed: {
-      confirmText () {
-        return (this.confirmed ? 'Отозвать' : 'Подтвердить')
+      confirmText (i) {
+        return (this['confirmed' + i] ? 'Отозвать' : 'Подтвердить')
       }
     },
 
